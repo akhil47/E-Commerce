7d1e2b363f5b42ef88324675239164bf01702fd7
Akhilesh L <akhil47@icloud.com>
2019-04-02 16:00:04 +0530

    Wishlist-page completed

--- src/app/app-routes.module.ts
+++ src/app/app-routes.module.ts
@@ -4,12 +4,16 @@ import { HomePageComponent } from './Pages/home-page/home-page.component';
 import { SearchPageComponent } from './Pages/search-page/search-page.component';
 import { ProductPageComponent } from './Pages/product-page/product-page.component';
 import { LoginRegisterPageComponent } from './Pages/login-register-page/login-register-page.component';
+import { WishlistPageComponent } from './Pages/wishlist-page/wishlist-page.component';
+import { CartPageComponent } from './Pages/cart-page/cart-page.component';
 
 const appRoutes = [
     { path: '', component: HomePageComponent},
     { path: 'search', component: SearchPageComponent},
     { path: 'product', component: ProductPageComponent},
-    { path: 'register', component: LoginRegisterPageComponent}
+    { path: 'register', component: LoginRegisterPageComponent},
+    { path: 'wishlist', component: WishlistPageComponent},
+    { path: 'cart', component: CartPageComponent}
 ]
 
 @NgModule({
