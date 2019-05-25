69b60bb9cdf6ad76ffaf66882fd74d95fef835f3
Akhilesh L <akhil47@icloud.com>
2019-05-25 15:01:13 +0530

    demo release v1.0

--- src/app/Pages/cart-page/cart-page.component.ts
+++ src/app/Pages/cart-page/cart-page.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-cart-page',
@@ -7,9 +8,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CartPageComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router, private route: ActivatedRoute) { }
 
   ngOnInit() {
   }
 
+  onPlaceOrder(){
+    this.router.navigate(['/address'])
+  }
 }
