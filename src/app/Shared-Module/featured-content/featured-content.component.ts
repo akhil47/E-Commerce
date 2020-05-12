7b1c98e2f4f87a26ea931732abf16d68ec33535f
Akhil47 <akhil47@icloud.com>
2020-05-12 23:07:43 +0530

    footer redesign

--- src/app/Shared-Module/featured-content/featured-content.component.ts
+++ src/app/Shared-Module/featured-content/featured-content.component.ts
@@ -1,12 +1,17 @@
-import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef, ViewChildren, QueryList, AfterViewInit, Renderer2 } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-featured-content',
   templateUrl: './featured-content.component.html',
   styleUrls: ['./featured-content.component.css']
 })
-export class FeaturedContentComponent implements OnInit {
+export class FeaturedContentComponent implements OnInit, AfterViewInit {
 
+  @ViewChild('imageSlider') slider : ElementRef
+  @ViewChildren('image') images : QueryList<ElementRef>
+
+  // Use different images for desktop and mobile versions
   imageList: string[] = [
     "https://images.thesouledstore.com/public/theSoul/uploads/slider/20190301172946.jpg",
     "https://images.thesouledstore.com/public/theSoul/uploads/slider/20190314140013.jpg",
@@ -14,49 +19,66 @@ export class FeaturedContentComponent implements OnInit {
     "https://images.thesouledstore.com/public/theSoul/uploads/slider/20190305102901.jpg",
     "https://images.thesouledstore.com/public/theSoul/uploads/slider/20190124122644.jpg"
   ]
+  linksList = ['search', 'search', 'search', 'search', 'search']
+
+  firstClone: string
+  firstCloneLink: string
+
+  lastClone: string
+  lastCloneLink: string
 
   currentImage: string
-  counter: number = 0
+  counter: number = 1
+  size : number = 0
 
-  constructor() {
-    this.currentImage = this.imageList[this.counter]
-    setInterval(()=>{
-      if(this.counter < 4){
-        this.counter++
-      }
-      else{
-        this.counter = 0
-      }
-      this.currentImage = this.imageList[this.counter]
-    }, 5000)
-    
+  constructor(private renderer: Renderer2, private router: Router) {
+    setInterval(()=>{ this.nextSlide() }, 5000)
+    this.firstClone = this.imageList[0]
+    this.firstCloneLink = this.linksList[0]
+    this.lastClone = this.imageList[this.imageList.length - 1]
+    this.lastCloneLink = this.linksList[this.linksList.length - 1]
   }
   ngOnInit() {
+    
+  }
+  ngAfterViewInit(){
+    this.size = this.slider.nativeElement.offsetWidth
+    this.renderer.setStyle(this.slider.nativeElement, 'transform', 'translateX(' + (-this.size * this.counter) + 'px)')
+  }
+  
+  imageLinkClicked(link: string){
+    this.router.navigate(['/' + link])
   }
 
-  activateSlide(index){
-    this.counter = index
-    this.currentImage = this.imageList[index]
+  previousSlide(){
+    if(this.counter <=0) return
+    this.counter -= 1;
+    this.applyTransition('transform 0.4s ease-in-out')
   }
-
-  backwardSlide(){
-    if(this.counter == 0){
-      this.counter = 4
-    }
-    else{
-      this.counter -= 1
-    }
-    this.currentImage = this.imageList[this.counter]
+  nextSlide(){
+    if(this.counter >= this.images.length - 1) return
+    this.counter += 1
+    this.applyTransition('transform 0.4s ease-in-out')
   }
+  transitionEnd($event){
+    // After each sliding transtion end(when sliding effect ends) it checks 
+    // wheather we have reached last slide or first slide
+    // if we reached first slide and try to go back then 
+    // lastClone is used to continue animation and is later reseted
+    // to original last slide, similarly for the first slide.
 
-  forwardSlide(){
-    if(this.counter == 4){
-      this.counter = 0
+    if(this.images['_results'][this.counter].nativeElement.id == 'lastClone'){
+      this.counter = this.images.length - 2
+      this.applyTransition('none')
     }
-    else{
-      this.counter += 1
+    if(this.images['_results'][this.counter].nativeElement.id == 'firstClone'){
+      this.counter = this.images.length - this.counter
+      this.applyTransition('none')
     }
-    this.currentImage = this.imageList[this.counter]
+  }
+  applyTransition(transitionType: string){
+    this.renderer.setStyle(this.slider.nativeElement, 'transition', transitionType)
+    this.renderer.setStyle(this.slider.nativeElement, 'transform', 'translateX(' + (-this.size * this.counter) + 'px)')
   }
 
   
